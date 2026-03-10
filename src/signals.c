85adc5aca38521a8c3324d40147587de7974c794
GANGULA SAI KIRAN REDDY <saikiranreddy19565@gmail.com>
2026-03-10 16:09:38 +0100

    feat: implement signals.c and jobs tracking, enhance main.c for background jobs

--- src/signals.c
+++ src/signals.c
@@ -1,12 +1,51 @@
 #include "shell.h"
 
-void setup_signals(void)
-{
-    /* TODO: Set up signal handlers (SIGINT, SIGTSTP, SIGCHLD) */
-}
-
+/**
+ * signal_handler() - Handle signals for the shell
+ * 
+ * SIGCHLD: Reap background/stopped child processes
+ * (SIGINT and SIGTSTP are ignored by parent shell)
+ */
 void signal_handler(int sig)
 {
-    (void)sig; /* Parameter unused in stub */
-    /* TODO: Handle signals */
+    if (sig == SIGCHLD) {
+        /* Handle child process state changes */
+        pid_t pid;
+        while ((pid = waitpid(-1, NULL, WNOHANG)) > 0) {
+            /* Child process finished or stopped */
+            Job *job = find_job_by_pid(pid);
+            if (job) {
+                remove_job(pid);
+            }
+        }
+    }
 }
+
+/**
+ * setup_signals() - Initialize signal handlers for the shell
+ * 
+ * - Shell ignores SIGINT (Ctrl+C) and SIGTSTP (Ctrl+Z)
+ *   so user input doesn't accidentally kill the shell
+ * - Child processes inherit default signal handlers
+ * - Sets up SIGCHLD handler to reap background processes
+ */
+void setup_signals(void)
+{
+    struct sigaction sa;
+
+    /* Ignore SIGINT (Ctrl+C) in the shell */
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = SIG_IGN;
+    sigaction(SIGINT, &sa, NULL);
+
+    /* Ignore SIGTSTP (Ctrl+Z) in the shell */
+    sigaction(SIGTSTP, &sa, NULL);
+
+    /* Set up SIGCHLD handler to reap background processes */
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;  /* Restart interrupted system calls */
+    sa.sa_handler = signal_handler;
+    sigaction(SIGCHLD, &sa, NULL);
+}
+
